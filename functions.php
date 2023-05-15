5cba0617244cd07424a53b9ced027a24eff7f502
Dadan Hidayat <dadanhidyt@gmail.com>
2023-05-15 03:13:10 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,2 +1,14 @@
 <?php
 include get_template_directory()."/classess/Post.php";
+function load_partial( string $filename ) : void {
+    $file = get_template_directory()."/template_part//".$filename.'.php';
+    if ( file_exists($file) ) {
+        require_once $file;
+    }
+}
+function load_page( string $filename ) : void {
+    $file = get_template_directory()."/pages//".$filename.'.php';
+    if ( file_exists($file) ) {
+        require_once $file;
+    }
+}
