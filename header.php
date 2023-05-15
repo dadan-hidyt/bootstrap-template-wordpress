06653976ee8dfdde1ba4eb918f8f825e50bafbc9
Dadan Hidayat <dadanhidyt@gmail.com>
2023-05-15 03:13:16 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="<?php bloginfo('language') ?>">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php wp_head(); ?>
+    <title> </title>
+  </head>
+  <body class="<?php get_body_class('app'); ?>">
+
+  <?php load_partial('navbar') ?>
