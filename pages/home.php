e6e59d791a8d8ac099920fe2806c1fdd0b9b9ad6
Dadan Hidayat <dadanhidyt@gmail.com>
2023-05-15 03:24:34 -0700

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -6,16 +6,22 @@
     <h3>POSTINGAN TERBARU</h3>
 
     <?php if ($posts = $post->getAll()) : ?>
-        <div class="row">
+        <div class="row g-3">
            <?php foreach($posts as $item): ?>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><?= get_the_title($item) ?></h5>
-                    <p class="card-text"><?= get_the_excerpt($item) ?></p>
-                    <a href="<?= get_the_permalink($item) ?>" class="btn btn-primary">Go somewhere</a>
+
+            <div class="col-12 col-md-4">
+            <div class="p-2">
+                <div class="card" style="width: 18rem;">
+                    <!-- <img src="..." class="card-img-top" alt="..."> -->
+                    <div class="card-body">
+                        <h5 class="card-title"><?= get_the_title($item) ?></h5>
+                        <p class="card-text"><?= get_the_excerpt($item) ?></p>
+                        <a href="<?= get_the_permalink($item) ?>" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
+            </div>
+            
             <?php endforeach; ?>
         </div>
     <?php endif; ?>
